63882ca419b3a3e06f9f42874a5c2098fa044d8d
Deft <khubaib.op@gmail.com>
2024-05-02 13:54:28 -0700

    chromium added

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
+import chromium from '@sparticuz/chromium'
 
 type Data = {
   message: string;
@@ -12,32 +13,34 @@ export default function handler(
 ) {
 
 
-  if(req.method === 'POST') {
+  if (req.method === 'POST') {
 
     (async () => {
-
-      const browser = await puppeteer.launch({
-        args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-dev-shm-usage',
-          '--disable-gpu',
-        ]
-      });
-      const page = await browser.newPage();
-
-      await page.goto(req.body.baseUrl, {
-        waitUntil: 'networkidle0',
-      });
-
-      await page.setViewport({ width: 1080, height: 1024 });
-      const pdf: any = await page.pdf({
-        format: 'A4',
-      });
-
-      await browser.close();
-      
-      res.status(200).send(pdf);
+      // try {
+        const browser = await puppeteer.launch({
+          args: chromium.args,
+          defaultViewport: chromium.defaultViewport,
+          executablePath: await chromium.executablePath(),
+          headless: chromium.headless,
+          ignoreHTTPSErrors: true,
+        });
+        const page = await browser.newPage();
+  
+        await page.goto(req.body.baseUrl, {
+          waitUntil: 'networkidle0',
+        });
+  
+        await page.setViewport({ width: 1080, height: 1024 });
+        const pdf: any = await page.pdf({
+          format: 'A4',
+        });
+  
+        await browser.close();
+  
+        res.status(200).send(pdf);
+      // } catch (error) {
+      //   res.status(200).json({ message: error });
+      // }
     })()
   } else {
     res.status(405).json({ message: 'Method not allowed' });
