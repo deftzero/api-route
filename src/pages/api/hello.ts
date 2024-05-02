990f96678ed9578fe329a437634dcf7bad31516f
Deft <khubaib.op@gmail.com>
2024-05-02 10:21:13 -0700

    init 2

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -9,5 +9,13 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+
+  // @ts-ignore: Nextjs API APP_URL: Update it once env contains the app url
+  const nextRequestMeta = req[Reflect.ownKeys(req).find(
+    (s) => String(s) === "Symbol(NextRequestMeta)"
+  )];
+
+  const baseUrl = nextRequestMeta.__NEXT_INIT_URL.split('/api/')[0]
+
+  res.status(200).json({ name: baseUrl });
 }
