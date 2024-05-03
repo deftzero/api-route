17de45d3b310e4dacec7df5ccb31e3a30f740b81
Deft <khubaib.op@gmail.com>
2024-05-03 04:31:05 -0700

    fetch pdf agreement

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -15,7 +15,9 @@ export default function handler(
 
   if (req.method === 'POST') {
 
+    
     (async () => {
+      const urlPage = "https://adl-qa-frontend.softyware.com?agreementCode=1first_name=Mayo&last_name=Maris&address1=33 West Rocky Old Parkway&city=Ducimus&state=Tempore&zip=76983&phone_number=+1 (123) 299-9329&emergency_contact_first_name=September&emergency_contact_last_name=Best&emergency_contact_address1=798 North Nobel Freeway&emergency_contact_city=Dolor&emergency_contact_state=Eu&emergency_contact_zip=11866&emergency_contact_phone=+1 (661) 853-9361&felony_conviction=0&signature=Test"
       // try {
         const browser = await puppeteer.launch({
           args: chromium.args,
@@ -26,7 +28,9 @@ export default function handler(
         });
         const page = await browser.newPage();
   
-        await page.goto('https://www.google.com');
+        await page.goto(urlPage, {
+          waitUntil: 'networkidle0'
+        });
   
         await page.setViewport({ width: 1080, height: 1024 });
         const pdf: any = await page.pdf({
@@ -34,7 +38,8 @@ export default function handler(
         });
   
         await browser.close();
-  
+        
+        res.setHeader('Content-Type', 'application/pdf')
         res.status(200).send(pdf);
       // } catch (error) {
       //   res.status(200).json({ message: error });
